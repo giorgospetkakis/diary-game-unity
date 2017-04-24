7b2314267da5149104aefbf88a70391d25c95df5
giorgospetkakis <giorgospetkakis@gmail.com>
2017-04-24 15:15:59 -0400

    Added text cue system

---
+++ Assets/Scripts/text/FileReader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class FileReader {
+	static string filename = "script.txt";
+	static Queue<string> scriptSequence = new Queue<string>();
+	public static Queue<string> getScript(){
+		StreamReader sr = new StreamReader(Application.dataPath + "/" + filename);
+		string contents = sr.ReadToEnd();
+		sr.Close();
+
+		string[] split = contents.Split('_');
+		for(int i = 0; i < split.Length; i++){
+			Debug.Log(split[i]);
+			scriptSequence.Enqueue(split[i]);
+		}
+
+		return scriptSequence;
+	}
+}
---
+++ Assets/Scripts/text/TextCue.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextCue : MonoBehaviour {
+
+	ArrayList history;
+	Queue<string> cuedText;
+	Text t;
+	// Use this for initialization
+	void Awake () {
+		cuedText = FileReader.getScript();
+		history = new ArrayList();
+		t = GetComponent<Text>();
+	}
+
+	void Start() {
+		showNextLine();
+		showNextLine();
+	}
+
+	public void showNextLine() {
+		string line = cuedText.Peek();
+		cuedText.Dequeue();
+		t.text = t.text + line;
+	}
+
+	public void clearPanel() {
+		history.Add(new string (t.text.ToCharArray()));
+		t.text = "";
+	}
+}
