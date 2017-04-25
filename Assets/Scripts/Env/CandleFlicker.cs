3778be9bf461d362c65ce54518872205bf0d7901
giorgospetkakis <giorgospetkakis@gmail.com>
2017-04-24 23:45:39 -0400

    Asset Changes

---
+++ Assets/Scripts/Env/CandleFlicker.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CandleFlicker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float i = GetComponent<Light>().intensity;
+		GetComponent<Light>().intensity = Mathf.Max(1.3f, (Mathf.Min ( 1.8f, Mathf.Lerp(i, i + Random.Range(-0.2f,0.2f), 0.1f))));
+	}
+}
