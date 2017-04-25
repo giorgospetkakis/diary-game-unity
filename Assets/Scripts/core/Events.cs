cc8483cbe196a413575840ae900f853217fa22aa
giorgospetkakis <giorgospetkakis@gmail.com>
2017-04-24 23:00:30 -0400

    Created Test Build

--- Assets/Scripts/core/Events.cs
+++ Assets/Scripts/core/Events.cs
@@ -1,23 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Events : MonoBehaviour {
 
 	EventQueue q;
-
+	int lineBlockIndex;
+	int[] blockSize;
 
 	// Use this for initialization
 	void Start () {
 		q = GetComponent<EventQueue>();
+		blockSize = new int[] {34};
+		lineBlockIndex = 0;
+
+		q.loadNextCutScene();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 			// INTRODUCTION
-			if(q.linesSinceCheckpoint == 34) {
+			if(q.linesSinceCheckpoint == blockSize[lineBlockIndex]) {
+				q.despawnCutscene();
 				q.loadNextMiniGame();
 				q.setLineCheckpoint();
 			}
+	}
+
+	void moveToNextBlock() {
+		lineBlockIndex++;
 	}
 }
